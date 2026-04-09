bb62563e96d27599f0407bb522aa8c6dbe9db81e
max larner <maxlarner@maxs-MacBook-Air-8.local>
2026-04-09 15:06:32 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://maxlarner.github.io',
+  base: '/GIS-Portfolio-Max-Larner-v2',
+  output: 'static',
+});
